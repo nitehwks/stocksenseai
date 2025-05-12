f7475faf5a9bd9aa3e099164b2425b2cb0d8d897
nitehwks <nitehwks@gmail.com>
2025-05-11 20:40:57 -0500

    Initial Commit

---
+++ StockSenseAI/StockSenseAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockSenseAIApp.swift
+//  StockSenseAI
+//
+//  Created by Joe Abbott on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct StockSenseAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StockSenseAITests/StockSenseAITests.swift
@@ -0,0 +1,17 @@
+//
+//  StockSenseAITests.swift
+//  StockSenseAITests
+//
+//  Created by Joe Abbott on 5/11/25.
+//
+
+import Testing
+@testable import StockSenseAI
+
+struct StockSenseAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
